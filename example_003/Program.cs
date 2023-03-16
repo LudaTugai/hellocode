84dcfe8db37bf87f876d1f78bc14365c865a03bd
Luda <you@example.com>
2023-03-16 22:53:44 +0300

    add random

--- example_003/Program.cs
+++ example_003/Program.cs
@@ -1,7 +1,10 @@
-﻿int A = 8;
-int B = 5;
+﻿int A = new Random().Next(1,7);
+Console.WriteLine(A);
+int B = new Random().Next(1,7);
+Console.WriteLine(B);
 int C = (A+B);
 Console.WriteLine(C);
+
 
 
 
